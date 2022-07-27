fe17262d1d9874f6be51640cfe5c0d0687808a75
Eric Oliveira <oliveiras.eric2012@gmail.com>
2022-07-27 14:24:36 -0300

    Aula 49, 50 - Serviçes

---
+++ src/app/tarefas/shared/tarefa.service.spec.ts
@@ -0,0 +1,17 @@
+import { Tarefa } from './tarefa.model';
+import { TestBed } from '@angular/core/testing';
+
+import { TarefaService } from './tarefa.service';
+
+describe('TarefaService', () => {
+  let service: TarefaService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TarefaService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
