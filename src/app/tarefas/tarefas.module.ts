b14c70c1460e92cebb7d7c850d3164718fcd31cd
Eric Oliveira <oliveiras.eric2012@gmail.com>
2022-07-28 16:05:22 -0300

    Create "editar-tarefa" component

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -6,12 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
-import { EditarTarefaComponent } from './editar-tarefa/editar-tarefa.component';
+import { EditarTarefaComponent } from './editar/editar-tarefa.component';
 
 @NgModule({
   declarations: [
     ListarTarefaComponent,
     CadastrarTarefaComponent,
+    EditarTarefaComponent
   ],
   imports: [
     CommonModule,
